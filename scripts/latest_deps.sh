584fb8330902edf0eaf1b8f9fbfd60b51aef3f00
Audrey Dutcher <audrey@rhelmot.io>
2019-02-27 13:20:30 -0800

    HHHHHHH

--- scripts/latest_deps.sh
+++ scripts/latest_deps.sh
@@ -12,6 +12,6 @@ git -C wheels reset --hard origin/${BRANCH_NAME-master}
 sudo apt update
 sudo apt upgrade -y
 sudo apt install -y vim build-essential python3 python3-dev python3-pip python3-setuptools zip git libffi-dev libtool libtool-bin wget automake bison cmake nasm clang libglib2.0-dev socat libc6:i386 libgcc1:i386 libstdc++6:i386 libtinfo5:i386 zlib1g:i386
-sudo pip3 install virtualenv pygithub
+sudo -H pip3 install virtualenv pygithub
 
 echo 'hello friends :)'
