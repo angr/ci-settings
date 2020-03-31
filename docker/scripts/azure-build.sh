6abc1934acb00512858be45147a2b36ddd27c7d2
Audrey Dutcher <audrey@rhelmot.io>
2020-03-31 01:01:30 -0700

    Remove some unneeded files from the build tarball

--- docker/scripts/azure-build.sh
+++ docker/scripts/azure-build.sh
@@ -22,4 +22,9 @@ else
     $SCRIPTS/discover_tests.py --repo $BUILD_REPOSITORY_URI --config $CONF --src ./src --eval-attribute 'speed != "slow"' > tests.txt
 fi
 cd ..
+
+# remove some unneeded files to reduce bloat
+rm -rf build/src/angr/.eggs build/src/vex/priv/*.o build/src/vex/libvex.a build/src/pyvex/pyvex_c/*.o build/src/pyvex/.eggs build/virtualenv/lib/python3.6/lib/site-packages/unicorn/lib/libunicorn.a build/virtualenv/lib/python3.6/lib/site-packages/babel/locale-data build/virtualenv/lib/python3.6/bin/z3
+
+# export
 tar -czf build.tar.gz build/src build/virtualenv build/tests.txt
