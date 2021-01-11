1e305bfd02e427bd30d2cd1ca0dbc1bb003acae5
Kevin Phoenix <kevin@kphoenix.us>
2021-01-11 16:55:55 -0700

    Use repos artifact when building angr-doc

--- scripts/build_api_docs.sh
+++ scripts/build_api_docs.sh
@@ -6,21 +6,6 @@ source $(dirname $0)/vars.sh
 source angr_venv/bin/activate
 pip install -r etc/doc_requirements.txt
 
-starting_dir="$(pwd)"
-
-mkdir -p $CHECKOUT_DIR
-pushd $CHECKOUT_DIR
-
-git clone git@github.com:angr/angr-doc.git angr-doc
-
-# In a real deployment, checkout the correct version
-# This is not doable in other conditions because we don't push to github
-angr_doc_rev="$(cat $starting_dir/release.yml | grep angr-doc | cut -d ' ' -f2)"
-if [ "$DRY_RUN" == "false" ]; then
-    git -C angr-doc reset --hard $angr_doc_rev
-fi
-angr_doc_version=$(sed -n -e "s/.*version = u'\(.\+\)'.*/\1/p" angr-doc/api-doc/source/conf.py)
-
 # Congifure path for sphinx
 modules="$(cd angr-doc/api-doc/source; ls *.rst | sed -r "s/\.rst//g; s/(\s|^)index(\s|$)//g")"
 for module in $modules; do
@@ -29,6 +14,4 @@ for module in $modules; do
 done
 
 # Build docs
-make -C angr-doc/api-doc html
-
-popd
+make -C repos/angr-doc/api-doc html
