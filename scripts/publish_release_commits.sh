59a61fa8b0e7f9fa260bf8715b01cb3584894e2b
Kevin Phoenix <kevin@kphoenix.us>
2021-01-12 11:59:04 -0700

    Fix typo in publish_release_commits.sh

--- scripts/publish_release_commits.sh
+++ scripts/publish_release_commits.sh
@@ -6,6 +6,6 @@ source "$(dirname "$0")/vars.sh"
 
 for i in $REPOS; do
     if [ "$DRY_RUN" == "false" ]; then
-        git -C $CEHCKOUT_DIR/$i push origin "v$VERSION"
+        git -C $CHECKOUT_DIR/$i push origin "v$VERSION"
     fi
 done
