b64b2963e9efc8cda36b04cd3f9d8a0208a0e623
Kevin Phoenix <kevin@kphoenix.us>
2021-01-12 21:31:17 -0700

    Don't depend on python for repo names

--- scripts/vars.sh
+++ scripts/vars.sh
@@ -1,4 +1,4 @@
 VERSION_ID="$BUILD_BUILDID"
-REPOS="$("$python" scripts/get_repo_names.py)"
+REPOS=(binaries vex angr-doc archinfo pyvex cle claripy ailment angr angr-management angrop)
 CHECKOUT_DIR=repos
 DOC_REQUIREMENTS=(sphinx sphinx_rtd_theme)
