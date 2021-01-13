2e32f47bdaf7c9af725ce4fbf2f7b659d1cd475f
Kevin Phoenix <kevin@kphoenix.us>
2021-01-13 10:53:55 -0700

    Fix issue in publishing release tags

--- scripts/publish_release_commits.sh
+++ scripts/publish_release_commits.sh
@@ -4,7 +4,8 @@ set -ex
 source "$(dirname "$0")/vars.sh"
 
 for i in $(ls $CHECKOUT_DIR); do
+    ref_to_push=$(git -C $CHECKOUT_DIR/$i describe --tags | head -n 1)
     if [ "$DRY_RUN" == "false" ]; then
-        git -C $CHECKOUT_DIR/$i push origin "v$VERSION"
+        git -C $CHECKOUT_DIR/$i push origin "$ref_to_push"
     fi
 done
