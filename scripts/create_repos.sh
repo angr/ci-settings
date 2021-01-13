6793578f2e89b589be8aa2892ecec2c8b1e8acef
Kevin Phoenix <kevin@kphoenix.us>
2021-01-12 21:36:48 -0700

    Replace checkout_all.py with create_repos.sh

---
+++ scripts/create_repos.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+source $(dirname $0)/vars.sh
+
+mkdir -p "repos"
+echo "!.git" > repos/.artifactignore
+
+for r in ${REPOS[@]}; do
+    git clone git@github.com:angr/$r.git $CHECKOUT_DIR/$r --depth=1
+done
