2317f5cf12359b827f0f18a87390dac0bea65a63
Kevin Phoenix <kevin@kphoenix.us>
2021-01-12 21:00:38 -0700

    Remove crazy regex since pip isn't as dumb anymore

--- scripts/prepare_venv_from_artifact.sh
+++ scripts/prepare_venv_from_artifact.sh
@@ -1,11 +1,10 @@
 set -ex
 
 python="$1"
-sdist_path="$2"
+dist_path="$2"
 venv_path="$3"
 
-packages="$("$python" scripts/get_repo_names.py --python-only | sed -E "s#[_-]#*#g; s#([^[:space:]]+)#$sdist_path/\1-[0-9]*#g")"
 "$python" -m venv "$venv_path"
 source "$venv_path/bin/activate" &> /dev/null || source "$venv_path/Scripts/activate"
 python -m pip install --upgrade pip wheel
-python -m pip install $packages
+python -m pip install $dist_path/*
