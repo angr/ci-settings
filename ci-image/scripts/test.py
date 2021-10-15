04bc3e0bf737b3be26ddf28cd6de621201aa1c15
Kevin Phoenix <kevin@kphoenix.us>
2021-10-14 17:20:44 -0700

    Use a thread pool to execute individual nose2 processes per test

--- ci-image/scripts/test.py
+++ ci-image/scripts/test.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 
 import collections
+from concurrent.futures import ThreadPoolExecutor, as_completed, CancelledError
+import multiprocessing
 import os
-import shutil
 import subprocess
 import sys
 
 
+# Reads tests file and returns a dict of project names to lists of tests
 def parse_tests(f):
     res = collections.defaultdict(list)
     for l in f.readlines():
@@ -15,11 +17,11 @@ def parse_tests(f):
     return res
 
 
-def test_project(project, tests, coverage=False):
-    coverage_flags = "--with-coverage --coverage ./src/{}".format(project)
+def run_single_test(project: str, test: str, coverage: bool = False):
+    coverage_flags = "--with-coverage --coverage ./src/{}".format(project) if coverage else ""
 
-    command = "nose2 -v -s ./src/{}/tests -c /root/config/nose2.cfg --log-level 100 {} {}".format(
-        project, coverage_flags if coverage else '', ' '.join(tests))
+    command = "nose2 -v -s ./src/{}/tests -c /root/config/nose2.cfg --log-level 100 --junit-xml-path {} {} {}".format(
+        project, os.path.join("results", project, test), coverage_flags, test)
 
     print("Running nose2 command:\n{}".format(command), flush=True)
     return subprocess.run(command, shell=True).returncode
@@ -48,18 +50,19 @@ def main():
 
     with open(test_file) as f:
         test_dict = parse_tests(f)
-        for k in test_dict:
-            rc = test_project(k, test_dict[k], coverage=coverage)
-            error_count += rc
-            with open("results/%s.returncode" % k, 'w') as rcf:
-                rcf.write(str(rc))
-                rcf.close()
 
-            if os.path.exists('/tmp/tests.xml'):
-                shutil.move('/tmp/tests.xml', "results/%s.tests.xml" % k)
+    with ThreadPoolExecutor(max_workers=multiprocessing.cpu_count()) as executor:
+        futures = []
+        for project, tests in test_dict.items():
+            os.makedirs(os.path.join("results", project))
+            for test in tests:
+                futures.append(executor.submit(run_single_test, project, test, coverage))
 
-            if os.path.exists("coverage.xml"):
-                shutil.move("coverage.xml", "results/%s.coverage.xml" % k)
+        for future in as_completed(futures):
+            try:
+                error_count += future.result()
+            except CancelledError:
+                error_count += 1
 
     sys.exit(error_count)
 
