fa9bb535352350dae8a92c7954c2417d3f988144
Kevin Phoenix <kevin@kphoenix.us>
2022-04-15 18:55:33 -0700

    Support un-deving versions for release

---
+++ scripts/versiontool.py
@@ -0,0 +1,43 @@
+import argparse
+import sys
+
+from packaging.version import Version, InvalidVersion
+
+
+def undev(version):
+    return version.base_version
+
+def bumpdev(version):
+    version = Version(version.base_version)
+    version_tuple = version.public.split(".")
+    version_tuple[-1] = str(int(version_tuple[-1]) + 1)
+    new_version = Version(".".join(version_tuple) + ".dev0")
+    return new_version.public
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Version Tool')
+    sp = parser.add_subparsers(dest='command')
+
+    undev_p = sp.add_parser("undev", help="Remove dev suffices from version")
+    undev_p.add_argument("version", help="Version to remove dev suffices from")
+
+    bumpdev_p = sp.add_parser("bumpdev", help="Bump version to next dev version")
+    bumpdev_p.add_argument("version", help="Version to bump to next dev version")
+
+    args = parser.parse_args()
+
+    try:
+        version = Version(args.version)
+    except InvalidVersion:
+        print("Invalid version: {}".format(args.version))
+        sys.exit(1)
+
+    if args.command == "undev":
+        print(undev(version))
+    if args.command == "bumpdev":
+        print(bumpdev(version))
+
+
+if __name__ == "__main__":
+    main()
