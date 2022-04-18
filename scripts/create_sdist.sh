47cc9ff78efd56586285006de181dc892e1985d9
Kevin Phoenix <kevin@kphoenix.us>
2022-04-18 16:13:58 -0700

    Add message when directory is skipped

--- scripts/create_sdist.sh
+++ scripts/create_sdist.sh
@@ -8,5 +8,7 @@ export PIP_FIND_LINKS="sdist"
 for i in $REPOS; do
     if [ -e "$CHECKOUT_DIR/$i/pyproject.toml" ]; then
         python -m build --sdist --outdir=sdist $CHECKOUT_DIR/$i
+    else
+        echo "Skipping $i"
     fi
 done
