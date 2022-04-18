03b2e9b757beabe6a83eb45a92fe78494cca64d6
Kevin Phoenix <kevin@kphoenix.us>
2022-04-18 16:07:48 -0700

    Use $REPOS order for build sdists to ensure dependencies are available

--- scripts/create_sdist.sh
+++ scripts/create_sdist.sh
@@ -5,8 +5,8 @@ source $(dirname $0)/vars.sh
 mkdir sdist
 
 export PIP_FIND_LINKS="sdist"
-for i in $(ls $CHECKOUT_DIR); do
-    if [ -e "$CHECKOUT_DIR/$i/setup.py" ]; then
+for i in $REPOS; do
+    if [ -e "$CHECKOUT_DIR/$i/pyproject.toml" ]; then
         python -m build --sdist --outdir=sdist $CHECKOUT_DIR/$i
     fi
 done
