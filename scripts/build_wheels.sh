68f4bc8b03eabfa4686c85be580850a3568c4a44
Kevin Phoenix <kevin@kphoenix.us>
2022-04-20 16:18:57 -0700

    Fix release

--- scripts/build_wheels.sh
+++ scripts/build_wheels.sh
@@ -7,14 +7,13 @@ function realpath() {
     [[ "$1" = /* ]] && echo "$1" || echo "$PWD/${1#./}"
 }
 
-python="$1"
-sdist_path="$(realpath "$2")"
+sdist_path="$(realpath "$1")"
 
-$python -m pip install build cibuildwheel==2.3.1
-
-platform="$($python -c "import distutils.util; print(distutils.util.get_platform())" | sed s'/-/_/g')"
-if [ "$(uname)" = "Linux" ]; then
-    platform="$(sed 's/linux/manylinux2010/' <<< "$platform")"
+python -m pip install build cibuildwheel==2.3.1
+if [ "$(uname)" == "Linux" ]; then
+    pip install auditwheel
+elif [ "$(uname)" == "Darwin" ]; then
+    pip install delocate
 fi
 
 wheels=$(realpath wheels)
@@ -29,14 +28,15 @@ for f in $(ls "$sdist_path"); do
 done
 
 export PIP_FIND_LINKS="$sdist_path"
-export CIBW_ENVIRONMENT_LINUX="/host$PIP_FIND_LINKS"
-export CIBW_ARCHS=native
+export CIBW_ENVIRONMENT_LINUX="PIP_FIND_LINKS=/host$PIP_FIND_LINKS"
+export CIBW_BUILD="cp36-manylinux_x86_64 cp36-win_amd64 cp36-macosx_x86_64"
+export CIBW_REPAIR_WHEEL_COMMAND=""
 for dist in $(ls); do
     package=$(cat $dist/PKG-INFO | grep '^Name: [a-zA-Z0-9-]\+$' | head -n 1 | cut -d' ' -f2)
     if is_native_package "$package"; then
-        $python -m cibuildwheel --output-dir "$wheels" $dist
+        python -m cibuildwheel --output-dir "$wheels" $dist
     elif [ "$(uname)" == "Linux" ]; then
-        $python -m build --wheel --outdir "$wheels" $dist
+        python -m build --wheel --outdir "$wheels" $dist
     fi
 done
 
