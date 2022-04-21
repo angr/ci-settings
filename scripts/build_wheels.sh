18d82af3e20d7dd9f5acb0ddf5fffb9d750af10d
Kevin Phoenix <kevin@kphoenix.us>
2022-04-20 18:46:45 -0700

    Simplify python usage in build_wheels.sh

--- scripts/build_wheels.sh
+++ scripts/build_wheels.sh
@@ -7,10 +7,9 @@ function realpath() {
     [[ "$1" = /* ]] && echo "$1" || echo "$PWD/${1#./}"
 }
 
-python="$1"
-sdist_path="$(realpath "$2")"
+sdist_path="$(realpath "$1")"
 
-$python -m pip install build cibuildwheel==2.3.1
+python -m pip install build cibuildwheel==2.3.1
 if [ "$(uname)" == "Linux" ]; then
     pip install auditwheel
 elif [ "$(uname)" == "Darwin" ]; then
@@ -34,9 +33,9 @@ export CIBW_BUILD="cp36-manylinux_x86_64 cp36-win_amd64 cp36-macosx_x86_64"
 for dist in $(ls); do
     package=$(cat $dist/PKG-INFO | grep '^Name: [a-zA-Z0-9-]\+$' | head -n 1 | cut -d' ' -f2)
     if is_native_package "$package"; then
-        $python -m cibuildwheel --output-dir "$wheels" $dist
+        python -m cibuildwheel --output-dir "$wheels" $dist
     elif [ "$(uname)" == "Linux" ]; then
-        $python -m build --wheel --outdir "$wheels" $dist
+        python -m build --wheel --outdir "$wheels" $dist
     fi
 done
 
