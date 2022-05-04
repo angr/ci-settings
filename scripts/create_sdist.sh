ac4d8992d02bda4d6efffed0e8c2c075179eb3c4
Kevin Phoenix <kevin@kphoenix.us>
2022-05-04 16:56:26 -0700

    Fix archr release artifacts not being built or published

--- scripts/create_sdist.sh
+++ scripts/create_sdist.sh
@@ -7,7 +7,7 @@ mkdir sdist
 python -m pip install build
 
 export PIP_FIND_LINKS="sdist"
-for i in $REPOS; do
+for i in $REPOS $REPOS_LINUX_ONLY; do
     if [ -e "$CHECKOUT_DIR/$i/pyproject.toml" ]; then
         python -m build --sdist --outdir=sdist $CHECKOUT_DIR/$i
     else
