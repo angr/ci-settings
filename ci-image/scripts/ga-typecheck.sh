d79a74aab8c2b9a046cda0368345bbbb410156c6
Audrey Dutcher <audrey@rhelmot.io>
2025-01-13 13:30:36 -0700

    fuck

--- ci-image/scripts/ga-typecheck.sh
+++ ci-image/scripts/ga-typecheck.sh
@@ -10,7 +10,7 @@ source virtualenv/bin/activate
 pip install pyright
 
 cd src/${GITHUB_REPOSITORY##*/}
-HEAD_REV="$(git rev-parse --abbrev-ref HEAD)"
+HEAD_REV="$(git rev-parse HEAD)"
 if [[ $GITHUB_REF == "master" ]]; then
     BASE_REV="$(git rev-parse --abbrev-ref HEAD~)"
 else
