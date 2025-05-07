015fb4832f8b92071a2e22738145921af8847c49
Kevin Phoenix <kevin@kphoenix.us>
2025-05-07 16:51:56 -0700

    Fold test.py into ga-test.sh

--- ci-image/scripts/ga-test.sh
+++ ci-image/scripts/ga-test.sh
@@ -22,4 +22,33 @@ fi
 cat tests.txt | awk "NR % $NUM_WORKERS == $WORKER" > todo.txt
 source virtualenv/bin/activate
 
-$SCRIPTS/test.py --tests todo.txt
+# Create results directory
+mkdir -p results
+ERROR_COUNT=0
+
+# Read test file line by line using readarray/mapfile
+readarray -t TESTS < todo.txt
+
+# Process each repo in a loop
+for LINE in "${TESTS[@]}"; do
+    PROJECT=$(echo $LINE | cut -d ' ' -f 1)
+    TESTS=$(echo $LINE | cut -d ' ' -f 2)
+
+    # Run the nose2 test command
+    echo "Running nose2 command for project $PROJECT: nose2 -v -s ./src/$PROJECT/tests -c /root/conf/nose2.cfg --log-level 100 $TESTS"
+    nose2 -v -s "./src/$PROJECT/tests" -c /root/conf/nose2.cfg --log-level 100 $TESTS
+    RC=$?
+
+    # Add to error count
+    ERROR_COUNT=$((ERROR_COUNT + RC))
+
+    # Save return code to results
+    echo "$RC" > "results/$PROJECT.returncode"
+
+    # Move test results if they exist
+    if [ -f "/tmp/tests.xml" ]; then
+        mv "/tmp/tests.xml" "results/$PROJECT.tests.xml"
+    fi
+done
+
+exit $ERROR_COUNT
