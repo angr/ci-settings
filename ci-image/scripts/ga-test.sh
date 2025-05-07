c8a7aa66b06c0358b31847ef637e43dbfe990f0d
Kevin Phoenix <kevin@kphoenix.us>
2025-05-07 16:40:11 -0700

    Remove unused coverage support

--- ci-image/scripts/ga-test.sh
+++ ci-image/scripts/ga-test.sh
@@ -21,8 +21,5 @@ fi
 
 cat tests.txt | awk "NR % $NUM_WORKERS == $WORKER" > todo.txt
 source virtualenv/bin/activate
-if [ "$1" == "nightly" ]; then
-    $SCRIPTS/test.py --tests todo.txt --coverage
-else
-    $SCRIPTS/test.py --tests todo.txt
-fi
+
+$SCRIPTS/test.py --tests todo.txt
--- ci-image/scripts/test.py
+++ ci-image/scripts/test.py
@@ -15,11 +15,12 @@ def parse_tests(f):
     return res
 
 
-def test_project(project, tests, coverage=False):
-    coverage_flags = "--with-coverage --coverage ./src/{}".format(project)
-
-    command = "nose2 -v -s ./src/{}/tests -c /root/conf/nose2.cfg --log-level 100 {} {}".format(
-        project, coverage_flags if coverage else '', ' '.join(tests))
+def test_project(project, tests):
+    command = (
+        "nose2 -v -s ./src/{}/tests -c /root/conf/nose2.cfg --log-level 100 {}".format(
+            project, " ".join(tests)
+        )
+    )
 
     print("Running nose2 command:\n{}".format(command), flush=True)
     return subprocess.run(command, shell=True).returncode
@@ -27,42 +28,36 @@ def test_project(project, tests, coverage=False):
 
 def main():
     test_file = None
-    coverage = False
 
     args = iter(sys.argv[1:])
 
     for arg in args:
-        if arg in ('--tests'):
+        if arg in ("--tests"):
             test_file = next(args)
-        elif arg in ('--coverage',):
-            coverage = True
         else:
             raise ValueError("Bad argument: %s" % arg)
 
         if test_file is None:
-            raise ValueError('Missing --tests')
+            raise ValueError("Missing --tests")
 
-    os.makedirs('results')
+    os.makedirs("results")
 
     error_count = 0
 
     with open(test_file) as f:
         test_dict = parse_tests(f)
         for k in test_dict:
-            rc = test_project(k, test_dict[k], coverage=coverage)
+            rc = test_project(k, test_dict[k])
             error_count += rc
-            with open("results/%s.returncode" % k, 'w') as rcf:
+            with open("results/%s.returncode" % k, "w") as rcf:
                 rcf.write(str(rc))
                 rcf.close()
 
-            if os.path.exists('/tmp/tests.xml'):
-                shutil.move('/tmp/tests.xml', "results/%s.tests.xml" % k)
-
-            if os.path.exists("coverage.xml"):
-                shutil.move("coverage.xml", "results/%s.coverage.xml" % k)
+            if os.path.exists("/tmp/tests.xml"):
+                shutil.move("/tmp/tests.xml", "results/%s.tests.xml" % k)
 
     sys.exit(error_count)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
