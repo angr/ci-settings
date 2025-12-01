f02f3a40f3b9f5fbd34302ba46677ca563a9fffd
Kevin Phoenix <kevin@kphoenix.us>
2025-12-01 14:24:07 -0700

    Inline many release scripts into release pipeline

--- release-scripts/publish_release_commits.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -ex
-
-for i in $(ls $CHECKOUT_DIR); do
-    ref_to_push=$(git -C $CHECKOUT_DIR/$i describe --tags | head -n 1)
-    if [ "$DRY_RUN" == "false" ]; then
-        git -C $CHECKOUT_DIR/$i push origin "$ref_to_push"
-    fi
-done
