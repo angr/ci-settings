0f68bb3f8894b778eb2ae2d9f4e48d089208663b
Kevin Phoenix <kevin@kphoenix.us>
2025-12-01 16:32:37 -0700

    Inline many release scripts into release pipeline (#87)
    * Inline many release scripts into release pipeline
    
    * Uninline create_release_commits.sh
    
    * Try fix
    
    * Fix output path
    
    * Fix windows path being double-escaped

--- release-scripts/checkout_repos.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-mkdir -p "repos"
-echo "!.git" > repos/.artifactignore
-
-for r in $REPOS; do
-    git clone git@github.com:angr/$r.git $CHECKOUT_DIR/$r --depth=1 --recursive
-done
