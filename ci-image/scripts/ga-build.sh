57269d1688b56b0ed5555c916d5e3c59a95ce216
Kevin Phoenix <kevin@kphoenix.us>
2026-05-12 11:07:59 -0700

    Merge install.sh into ga-build.sh

--- ci-image/scripts/ga-build.sh
+++ ci-image/scripts/ga-build.sh
@@ -1,20 +1,49 @@
-#!/bin/bash -ex
+#!/bin/bash
+set -ex
 
 BASEDIR=$(dirname $(dirname $0))
 SCRIPTS=$BASEDIR/scripts
 CONF=$BASEDIR/conf
-WHEELS=$BASEDIR/wheels
-
 
 git config --global url.https://github.com/.insteadOf git@github.com:
 
+
 mkdir build
 pushd build
+
 $SCRIPTS/resolve_refs.py $CONF . $GITHUB_REPOSITORY $GITHUB_REF
 echo "snapshot_branch=$(cat ./snapshot_branch.txt)" >> "${GITHUB_OUTPUT:-/dev/null}"
-$SCRIPTS/install.sh
 
-source virtualenv/bin/activate
+uv venv ./virtualenv --relocatable
+source ./virtualenv/bin/activate
+
+uv pip install --no-cache --requirement /root/conf/requirements.txt
+
+venv_install() {
+    uv pip install --no-sources "$@"
+}
+
+venv_install ./src/archinfo
+uv build ./src/pyvex  # angr will need the wheel
+venv_install ./src/pyvex
+venv_install ./src/claripy
+venv_install ./src/cle
+venv_install -f ./src/pyvex/dist ./src/angr[angrdb,llm,unicorn]
+venv_install ./src/angr-platforms
+venv_install ./src/pysoot
+venv_install ./src/tracer
+venv_install ./src/archr[qtrace]
+venv_install ./src/angr-management
+venv_install ./src/angrop
+venv_install ./src/phuzzer
+venv_install ./src/povsim
+venv_install -e ./src/compilerex --config-settings editable_mode=compat
+venv_install ./src/patcherex
+venv_install ./src/heaphopper
+venv_install ./src/driller
+venv_install ./src/rex
+venv_install ./src/colorguard
+
 
 # discover corpus tests
 IFS=';' CORPUS_TEST_PATHS_LIST=( ${CORPUS_TEST_PATHS-decompiler_corpus} )
